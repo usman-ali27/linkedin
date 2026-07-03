646cad97881c87fa4ef2eab0acccffa7f0ae4d97
faisalzaki966 <faisalzaki966@gmail.com>
2026-07-03 16:28:44 +0500

    client issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://linkedin-nu-ten.vercel.app"],
     credentials: true,
   }),
 );

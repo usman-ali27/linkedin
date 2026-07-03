6a32ab25bd8bb4d7369c8f4fe611ac91b97f64ea
faisalzaki966 <faisalzaki966@gmail.com>
2026-07-03 16:10:50 +0500

    prisma issue

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from "../generated/prisma/client.ts";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST!,

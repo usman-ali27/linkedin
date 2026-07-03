bdda64ab8044a8f612d8f07f9934bb1427572850
faisalzaki966 <faisalzaki966@gmail.com>
2026-07-03 17:07:04 +0500

    server issue

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -4,10 +4,14 @@ import { PrismaClient } from "../generated/prisma/client.ts";
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST!,
+  port: Number(process.env.DATABASE_PORT) || 3306,
   user: process.env.DATABASE_USER!,
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
   connectionLimit: 10,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 const prisma = new PrismaClient({ adapter });
